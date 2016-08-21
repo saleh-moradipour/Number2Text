faa605db3f01f504ded7ccde959f9c8163b45a83
Saleh <salehmoradipour91@gmail.com>
2016-08-21 19:16:11 +0430

    Project setup.

--- src/main/java/com/masal/Operation.java
+++ src/main/java/com/masal/Operation.java
@@ -5,8 +5,70 @@ package com.masal;
  */
 public class Operation {
     public String zBist(byte a) {
-        switch (a) {
+        switch (Integer.parseInt(String.valueOf(a))) {
             case 1:
+                System.out.println("یک");
+                break;
+            case 2:
+                System.out.println("دو");
+                break;
+            case 3:
+                System.out.println("سه");
+                break;
+            case 4:
+                System.out.println("چهار");
+                break;
+            case 5:
+                System.out.println("پنج");
+                break;
+            case 6:
+                System.out.println("شیش");
+                break;
+            case 7:
+                System.out.println("هفت");
+                break;
+            case 8:
+                System.out.println("هشت");
+                break;
+            case 9:
+                System.out.println("نه");
+                break;
+            case 10:
+                System.out.println("ده");
+                break;
+            case 11:
+                System.out.println("یازده");
+                break;
+            case 12:
+                System.out.println("دوازده");
+                break;
+            case 13:
+                System.out.println("سینزده");
+                break;
+            case 14:
+                System.out.println("چهارده");
+                break;
+            case 15:
+                System.out.println("پانزده");
+                break;
+            case 16:
+                System.out.println("شانزده");
+                break;
+            case 17:
+                System.out.println("هفده");
+                break;
+            case 18:
+                System.out.println("هجده");
+                break;
+            case 19:
+                System.out.println("نوزده");
+                break;
+            case 20:
+                System.out.println("بیست");
+                break;
+
         }
+
+        return String.valueOf(a);
     }
 }
