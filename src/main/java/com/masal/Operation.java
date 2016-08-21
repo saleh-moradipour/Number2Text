285c4a6eb3838e63905687547856ce646045de0c
Saleh <salehmoradipour91@gmail.com>
2016-08-21 18:54:31 +0430

    Start p.

--- src/main/java/com/masal/Operation.java
+++ src/main/java/com/masal/Operation.java
@@ -4,4 +4,9 @@ package com.masal;
  * Created by Yeganeh on 8/21/16.
  */
 public class Operation {
+    public String zBist(byte a) {
+        switch (a) {
+            case 1:
+        }
+    }
 }
